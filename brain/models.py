baee9ffd78770132a47d374b050ad5673e5c9d55
Varand Abrahamian <contact@varand.me>
2024-11-22 13:22:02 -0800

    add timestamps to messages / message history

--- brain/models.py
+++ brain/models.py
@@ -1,31 +1,29 @@
-from datetime import datetime
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
+from datetime import datetime
 
 
 class ChatMessage(BaseModel):
     from_creator: bool
     content: str
+    time_stamp: datetime = datetime.now()
 
     def __str__(self):
         role = "YOU" if self.from_creator else "THE FAN"
-        message = role + ": " + self.content
+        message = f"{role} : {self.content} sent at: {self.time_stamp.strftime("%Y-%m-%d %H:%M:%S")}"
         return message
+
 
 class ChatHistory(BaseModel):
     messages: List[ChatMessage] = []
+    created_at: datetime = datetime.now()
 
     def __str__(self):
         messages = []
-        for i, message in enumerate(self.messages):
+        for _, message in enumerate(self.messages):
             message_str = str(message)
-            # if i == len(self.messages) - 1 and not message.from_creator:
-            #     message_str = (
-            #         "(The fan just sent the following message which your message must respond to): "
-            #         + message_str
-            #     )
             messages.append(message_str)
-        return "\n".join(messages)
-    
+        return f"started_at: {self.created_at.strftime("%Y-%m-%d %H:%M:%S")}\n {"\n".join(messages)}"
+
     def model_dump_json(self, **kwargs):
         return str(self)
