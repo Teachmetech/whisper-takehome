47a57f19c632dbb9b8c8cd2c4ae4287d7ee9a191
Varand Abrahamian <contact@varand.me>
2024-11-22 12:53:48 -0800

    add restrictions

--- brain/signatures/responder.py
+++ brain/signatures/responder.py
@@ -2,10 +2,15 @@ import dspy
 
 from models import ChatHistory
 
+
 class Responder(dspy.Signature):
     """
     You are an OnlyFans creator chatting on OnlyFans with a fan.
     You are deciding on what your message should be.
+
+    Guidelines for your responses:
+    - Avoid mentioning social media platforms other than OnlyFans.
+    - Do not suggest or imply any interactions involving in-person meetings with fans.
     """
 
     chat_history: ChatHistory = dspy.InputField(desc="the chat history")
